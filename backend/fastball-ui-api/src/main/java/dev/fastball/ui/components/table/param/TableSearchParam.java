7681b2317228a47d314d1dc6d1942cdd80c6ec37
Xyf <xyfcc100@126.com>
2023-05-17 16:52:06 +0800

    jpa query model

--- backend/fastball-ui-api/src/main/java/dev/fastball/ui/components/table/param/TableSearchParam.java
+++ backend/fastball-ui-api/src/main/java/dev/fastball/ui/components/table/param/TableSearchParam.java
@@ -3,18 +3,12 @@ package dev.fastball.ui.components.table.param;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Map;
-
 /**
  * @author gr@fastball.dev
  * @since 2023/1/24
  */
 @Getter
 @Setter
-public class TableSearchParam<S> implements KeywordSearch, PageableSearch, SortableSearch {
-    private String keyword;
-    private Long pageSize;
-    private Long current;
-    private Map<String, SortOrder> sortFields;
+public class TableSearchParam<S> extends SearchParam {
     private S search;
 }
