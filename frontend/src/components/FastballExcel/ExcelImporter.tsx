8cd4d3bdb0152723e1ba0b025c56db71d2f4889e
GR <gr@fastball.dev>
2025-03-27 07:00:37 +0800

    feat: add excel importer history

--- frontend/src/components/FastballExcel/ExcelImporter.tsx
+++ frontend/src/components/FastballExcel/ExcelImporter.tsx
@@ -1,11 +1,102 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button, Modal } from "antd";
-import { buildAction, doApiAction } from "../../common";
-import { BasicComponentProps } from "../../../types";
+import { buildAction, doApiAction, FastballFieldProvider } from "../../common";
+import {
+  ApiActionInfo,
+  BasicComponentProps,
+  Data,
+  TableData,
+} from "../../../types";
+import { ActionType, ProTable, ProTableProps } from "@fastball/pro-components";
+
 
 const FastballExcelImporter: React.FC<BasicComponentProps> = ({
   componentKey,
+  container,
 }) => {
+
+
+  const ref = React.useRef<ActionType>();
+  const reloadData = () => {
+    ref.current?.reload();
+    setTimeout(() => {
+      reloadData();
+    }, 1000);
+  }
+
+  useEffect(() => {
+    reloadData();
+  }, []);
+
+  const props: ProTableProps<Data, any> = {
+    search: false,
+    columns: [
+      {
+        title: "导入时间",
+        dataIndex: "importTime",
+        valueType: "dateTime",
+      },
+      {
+        title: "导入文件",
+        dataIndex: "importFile",
+        valueType: "Attachment",
+      },
+      {
+        title: "状态",
+        dataIndex: "state",
+        valueType: "select",
+        valueEnum: {
+          SUCCESS: {
+            text: "导入成功",
+            color: "#00CC99",
+          },
+          PARTIAL_SUCCESS: {
+            text: "部分成功",
+            color: "#CC6600",
+          },
+          FAIL: {
+            text: "导入失败",
+            color: "#CC0000",
+          },
+          IMPORTING: {
+            text: "导入中",
+            color: "#00CCFF",
+          },
+        },
+      },
+      {
+        title: "导入总条数",
+        dataIndex: "totalCount",
+      },
+      {
+        title: "成功总条数",
+        dataIndex: "successCount",
+      },
+      {
+        title: "失败总条数",
+        dataIndex: "failCount",
+      },
+      {
+        title: "导入结果",
+        dataIndex: "resultFile",
+        valueType: "Attachment",
+      },
+    ],
+    request: async (params, sortFields) => {
+      const { pageSize, current, keyword } = params;
+      const searchParam = { sortFields, pageSize, current, keyword };
+      const apiActionInfo: ApiActionInfo = {
+        componentKey,
+        type: "API",
+        actionKey: "historyRecordList",
+        data: [searchParam],
+      };
+      const result: Data[] = await doApiAction(apiActionInfo);
+      return result;
+    },
+    rowKey: "id"
+  };
+
   const downloadTemplate = async () => {
     await doApiAction({
       componentKey,
@@ -23,9 +114,13 @@ const FastballExcelImporter: React.FC<BasicComponentProps> = ({
         componentKey,
         type: "API",
         actionKey: "importData",
+        actionName: "导入",
         uploadFileAction: true,
         data: [],
       })}
+      <FastballFieldProvider container={container}>
+        <ProTable<Data> actionRef={ref} {...props} />
+      </FastballFieldProvider>
     </>
   );
 };
